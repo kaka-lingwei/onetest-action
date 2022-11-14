120b798b70aeef9f34e4a6b3be722d4e80d85206
wangtong.wt <wangtong.wt@alibaba-inc.com>
2022-11-14 20:17:32 +0800

    add ask login

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,9 @@
 echo "Start test version: $2"
 
 mkdir -p ${HOME}/.kube
-echo $1 > ${HOME}/.kube/config
+echo '${1}' > ${HOME}/.kube/config
 cat ${HOME}/.kube/config
+export KUBECONFIG="${HOME}/.kube/config"
 
 pods=$(kubectl get pods -n a)
 echo "pods=pods" >> $GITHUB_OUTPUT
