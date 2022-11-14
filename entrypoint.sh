807dea84a8f9e35831682f42baeda673b0fd6af5
wangtong.wt <wangtong.wt@alibaba-inc.com>
2022-11-14 21:16:56 +0800

    add ask login

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,14 +3,14 @@
 echo "Start test version: $2"
 
 mkdir -p ${HOME}/.kube
-kube_config=$(echo $1 | base64 -d)
-echo $kube_config > ${HOME}/.kube/config
+kube_config=$(echo "${1}" | base64 -d)
+echo "${kube_config}" > ${HOME}/.kube/config
 export KUBECONFIG="${HOME}/.kube/config"
 
 wget https://gosspublic.alicdn.com/ossutil/1.7.14/ossutil64
 chmod 755 ossutil64
 ./ossutil64 config -e oss-us-west-1.aliyuncs.com -i $3 -k $4  -L CH
-./ossutil64 cp ${HOME}/.kube/config oss://onetest-opensource-oss/
+./ossutil64 cp -f ${HOME}/.kube/config oss://onetest-opensource-oss/
 
 pods=$(kubectl get pods -n a)
 echo "pods=pods" >> $GITHUB_OUTPUT
