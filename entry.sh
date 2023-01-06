a02e293a07fac4868cbd97ab33406ca2c69fff16
wangtong.wt <wangtong.wt@alibaba-inc.com>
2023-01-06 16:02:54 +0800

    add test code path and branch support

--- entry.sh
+++ entry.sh
@@ -129,7 +129,7 @@ spec:
     - name: onetest-regcred
   containers:
   - name: test-${ns}
-    image: cn-cicd-repo-registry.cn-hangzhou.cr.aliyuncs.com/cicd/test-runner:v0.0.4
+    image: cn-cicd-repo-registry.cn-hangzhou.cr.aliyuncs.com/cicd/test-runner:v0.0.5
     env:
     - name: CODE
       value: ${TEST_CODE_GIT}
